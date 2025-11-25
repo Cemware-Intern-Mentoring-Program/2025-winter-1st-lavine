79392e7ce2120fd86db8af501bd72ba6d9b23fee
SeonWu Kim <sunwoo005@naver.com>
2025-11-25 23:58:22 +0900

    Init: Project Setting

---
+++ app/src/test/java/com/cemware/lavine/LavineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cemware.lavine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LavineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
